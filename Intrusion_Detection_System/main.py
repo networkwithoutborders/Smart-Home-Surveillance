fa9d7e0819ceb30a0332b800eed513bfd9fa648b
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2021-10-31 01:53:37 +0530

    Added Intrusion Detection Feature

--- Intrusion_Detection_System/main.py
+++ Intrusion_Detection_System/main.py
@@ -9,6 +9,7 @@ while cap.isOpened():
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     bodies=body_classifire.detectMultiScale(gray,1.1,3)
 
+
     for (x,y,w,h) in bodies:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),5)
         cv2.putText(gray, "Intrusion Detected", (210, 190), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
